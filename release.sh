8d12e31b1e71cbf1b7bf753e9182d750542ac057
Henry Coggins <henry.coggins@tngtech.com>
2021-07-12 16:38:26 +0200

    feat: Release from special release branch
    Signed-off-by: Henry Coggins <henry.coggins@tngtech.com>

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# default: not dry-run
 DRY_RUN=0
 
 set -e
@@ -33,9 +34,19 @@ if [[ ! $GIT_BRANCH =~ main ]]; then
     exit 1
 fi
 
-echo Releasing version "$VERSION"
+git pull
 
-echo Updating version in package.json ...
+RELEASE_BRANCH="temp-release-branch-${VERSION_PREFIXED}"
+git branch "$RELEASE_BRANCH"
+git checkout "$RELEASE_BRANCH"
+
+if [[ $DRY_RUN ]]; then
+  echo "Releasing version $VERSION (dry-run)"
+else
+  echo "Releasing version $VERSION"
+fi
+
+echo "Updating version in package.json ..."
 sed -i -e "s/\"version\":.*/\"version\":\ \"$VERSION\"\,/" package.json
 npm install
 
@@ -46,26 +57,28 @@ if [ -n "$(git status --porcelain)" ]; then
     git commit --signoff -m "Update version to $VERSION"
 fi
 
-echo Linting, building and testing:
+echo "Linting, building and testing:"
 npm run lint
 npm run build
 npm run test
 
-echo Creating Tag...
+echo "Creating Tag ..."
 git tag -a -m "$VERSION_PREFIXED" "$VERSION_PREFIXED"
 
 if [[ $DRY_RUN ]]; then
-  echo "Pushing version and tag to GitHub repository (dry-run)":
-  git push --dry-run
+  echo "Pushing version and tag to GitHub repository (dry-run):"
+  git push --set-upstream origin "$RELEASE_BRANCH" --dry-run
   git push "$(git config --get remote.origin.url)" "$VERSION_PREFIXED" --dry-run
 
   echo "Publish to npmjs (dry-run):"
   npm publish --access public --registry https://registry.npmjs.org/ --dry-run
+
 else
   echo "Pushing version and tag to GitHub repository:"
-  git push
+  git push --set-upstream origin "$RELEASE_BRANCH"
   git push "$(git config --get remote.origin.url)" "$VERSION_PREFIXED"
 
   echo "Publish to npmjs:"
   npm publish --access public --registry https://registry.npmjs.org/
+
 fi
