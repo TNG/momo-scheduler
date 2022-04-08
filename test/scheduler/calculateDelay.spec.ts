dfb7b5438b088ad507e5e46de235b41ca1c2c95e
eickern <niklas.eicker@tngtech.com>
2022-04-08 11:46:07 +0200

    fix: Remove unnecessary describe block
    Signed-off-by: eickern <niklas.eicker@tngtech.com>

--- test/scheduler/calculateDelay.spec.ts
+++ test/scheduler/calculateDelay.spec.ts
@@ -7,31 +7,28 @@ import { calculateDelay } from '../../src/scheduler/calculateDelay';
 
 describe('calculateDelay', () => {
   const clock: Clock = install();
+  const job: JobEntity = {
+    name: 'test',
+    interval: 'one second',
+    firstRunAfter: 500,
+    concurrency: 0,
+    maxRunning: 1,
+  };
 
   afterAll(() => clock.reset());
 
-  describe('with delay', () => {
-    const job: JobEntity = {
-      name: 'test',
-      interval: 'one second',
-      firstRunAfter: 500,
-      concurrency: 0,
-      maxRunning: 1,
-    };
+  it('uses configured delay if job was never started before', () => {
+    const delay = calculateDelay(1000, job);
 
-    it('uses configured delay if job was never started before', () => {
-      const delay = calculateDelay(1000, job);
+    expect(delay).toBe(job.firstRunAfter);
+  });
 
-      expect(delay).toBe(job.firstRunAfter);
-    });
+  it('calculates delay based on lastStarted', () => {
+    job.executionInfo = { lastStarted: DateTime.now().toISO() } as ExecutionInfo;
 
-    it('calculates delay based on lastStarted', () => {
-      job.executionInfo = { lastStarted: DateTime.now().toISO() } as ExecutionInfo;
+    clock.tick(500);
 
-      clock.tick(500);
-
-      const delay = calculateDelay(1000, job);
-      expect(delay).toBe(500);
-    });
+    const delay = calculateDelay(1000, job);
+    expect(delay).toBe(500);
   });
 });
