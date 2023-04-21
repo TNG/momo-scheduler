7377ce07c31f567228498527aa99f0f3695cde44
eickern <niklas.eicker@tngtech.com>
2023-04-21 17:34:24 +0200

    fix: configure luxon with throwOnInvalid
    Signed-off-by: eickern <niklas.eicker@tngtech.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Settings } from 'luxon';
+
 export { MomoOptions, MongoSchedule } from './schedule/MongoSchedule';
 export { momoError } from './logging/error/MomoError';
 export { MomoErrorType } from './logging/error/MomoErrorType';
@@ -8,3 +10,11 @@ export { ExecutionStatus } from './job/ExecutionInfo';
 export { ExecutionInfo } from './job/ExecutionInfo';
 export { MomoJobBuilder } from './job/MomoJobBuilder';
 export { MongoScheduleBuilder } from './schedule/MongoScheduleBuilder';
+
+// We don't parse any dates that might be invalid, but only get the current date time
+Settings.throwOnInvalid = true;
+declare module 'luxon' {
+  interface TSSettings {
+    throwOnInvalid: true;
+  }
+}
